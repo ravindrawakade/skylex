92659a565da3996c294099b9b22395e7005d55b6
Admin <Admin@RAVI>
2022-05-04 01:07:12 +0530

    [Ravi] find middle char of string "Ravi"

---
+++ src/practice/prog.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class prog {
+
+	public static void main(String[] args)
+	{
+		String a = "RAVINDRA";
+		System.out.println(a.substring(a.length()/2-1,a.length()/2+1));
+		
+	}
+
+}
